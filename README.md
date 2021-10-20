cdbb95354da55235c081da8be16c2179b83194e8
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2021-10-20 21:35:02 +0100

    Basic working version.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # disk-watcher-mqtt
+
+A very simple example of a service that periodically reports disk usage stats to an MQTT broker. I wrote this because I needed to keep an eye on the amount of free space on several Raspberry Pis around the house (they are using 32GB SD cards). I haven't written in Python in years, but I choose the language because it's available on all the Pis, even the Zero. So this is unlikely to be a good example of how to do this. I just needed it to work, not be elegant.
+
+## Installation
+```
+sudo apt-get update
+sudo apt install git python3-pip -y
+git clone https://github.com/eamonnsullivan/disk-watcher-mqtt.git
+cd disk-watcher-mqtt
+
+# install the requirements
+pip3 install -r requirements.txt
+
+# edit the configuration
+nano disk-watcher.conf
+```
+## Configuration
+
+See `disk-watcher.conf`, set your broker, port, etc. You'll also want to set the topic_base (the first part of the topic) and the frequency (in seconds) of reporting.
+
+## Set as a service
+
+```
+sudo ln -s /home/pi/disk-watcher/disk-watcher.service /etc/systemd/system/
+sudo systemctl --system daemon-reload
+sudo systemctl enable disk-watcher.service
+sudo systemctl start disk-watcher.service
+# wait a few seconds
+sudo systemctl status disk-watcher.service
+```
---
+++ disk-watcher.py
@@ -0,0 +1,81 @@
+import time
+import sys
+from os import path
+import logging
+import paho.mqtt.client as mqtt
+from configparser import ConfigParser
+import shutil
+import socket
+import sdnotify
+
+LOGGING_FORMAT = '%(name)s %(asctime)-15s [%(levelname)s] %(message)s'
+logging.basicConfig(format=LOGGING_FORMAT)
+
+
+config = ConfigParser()
+HERE = path.dirname(path.realpath(__file__))
+config.read(path.join(HERE, 'disk-watcher.conf'))
+logger = logging.getLogger(name='disk-watcher')
+logger.setLevel(config['default'].get('loglevel', 'INFO').upper())
+
+systemd = sdnotify.SystemdNotifier()
+
+MQTT_BROKER = config['mqtt.broker']
+HOST = MQTT_BROKER['host']
+PORT = int(MQTT_BROKER['port'])
+KEEPALIVE = int(MQTT_BROKER['keepalive'])
+
+PUBLISHING = config['publishing']
+BASE_TOPIC = PUBLISHING['topic_base'] + "/" + socket.gethostname()
+FREQUENCY = int(PUBLISHING['frequency'])
+
+DISKS = list(config['filesystems'].values())
+
+logging.info('Monitoring the following file systems:')
+for disk in DISKS:
+    logging.info(disk)
+
+
+def on_connect(client, userdata, flags, rc):
+    logging.info("Connected with result code " + str(rc))
+
+
+MqttClient = mqtt.Client()
+MqttClient.on_connect = on_connect
+
+
+def publisher():
+    logger.info("Starting to read free disk space")
+    systemd.notify("READY=1")
+    while True:
+        for disk in DISKS:
+            total, used, free = shutil.disk_usage(disk)
+            logger.debug("Disk: {}, total: {}, used: {}, free: {}".format(
+                disk, total, used, free
+            ))
+            path = "/disk" + disk
+            MqttClient.connect(HOST, PORT, KEEPALIVE)
+            MqttClient.publish(BASE_TOPIC + path + "/total_gb", (total // (2**30)))
+            MqttClient.publish(BASE_TOPIC + path + "/used_gb", (used // (2**30)))
+            MqttClient.publish(BASE_TOPIC + path + "/free_gb", (free // (2**30)))
+            MqttClient.publish(BASE_TOPIC + path + "/used_pct", (used/total * 100))
+            MqttClient.disconnect()
+            systemd.notify("WATCHDOG=1")
+            time.sleep(FREQUENCY)
+
+
+def main():
+    try:
+        publisher()
+    except KeyboardInterrupt:
+        logger.info("Received keyboard interrupt. Quitting.")
+        systemd.notify("STOPPING=1")
+        sys.exit(0)
+    except Exception:
+        logger.exception("Unhandled exception during execution.", exc_info=True)
+        systemd.notify("STATUS=An exception occured.\nERRNO=1")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
